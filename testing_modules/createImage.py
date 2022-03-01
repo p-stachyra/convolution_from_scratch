9d251b2f6eabcc57e5110b146262c64a4c4c875a
Piotr <piotrstachyra1@gmail.com>
2022-03-01 22:31:55 +0100

    Module for loss function
    The module for loss function was added and used in main driver. The main module was changed to clarify functionality.

---
+++ testing_modules/createImage.py
@@ -0,0 +1,19 @@
+import cv2
+from PIL import Image
+
+WIDTH = 5
+HEIGHT = 5
+
+
+def createImage(width, height):
+    img = Image.new(mode="RGB", size=(width, height), color=(209, 123, 193))
+    img.save("../test_image.PNG")
+    return 0
+
+
+def readImage(filename):
+    img = cv2.imread(filename)
+    return img
+
+
+createImage(WIDTH, HEIGHT)
